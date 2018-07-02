00dee616adee0acb626085561154358c388e37aa
Amos Davis <amosdavis@Amoss-MacBook-Pro.local>
2018-07-02 10:27:33 -0500

    warmup js

--- js/warmup.js
+++ js/warmup.js
@@ -16,7 +16,6 @@ function sortedNameGenerator (input1arr, input2arr) {
     // for (var i = 0; i < input1arr.length, i < input2arr.length; i++) {
     //     namesArr = firstSorted[i].push() + ',' + lastSorted[i].push()
     // }
-
     firstSorted.forEach(function(element, index) {
         namesArr.push({firstName: firstSorted[index], lastName: lastSorted[index]})
     });
